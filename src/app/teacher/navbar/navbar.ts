e73af9b983b52fa277251d71e0c2b105300ae722
ianlabicani <labicaniian@gmail.com>
2025-08-21 15:27:24 +0800

    add logout button for teacher

--- src/app/teacher/navbar/navbar.ts
+++ src/app/teacher/navbar/navbar.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-teacher-navbar',
@@ -7,4 +8,12 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+}
