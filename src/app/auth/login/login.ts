f3b4650f561378de68738849f346d823a75676ac
ianlabicani <labicaniian@gmail.com>
2025-09-04 09:14:08 +0800

    add loading effect and better error message

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FaIconComponent],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -19,9 +21,15 @@ export class Login {
     password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
+  protected isSigningInSig = signal(false);
+  protected errorMessageSig = signal<string | null>(null);
+  protected faSpinner = faSpinner;
+
   protected onSubmit() {
+    this.isSigningInSig.set(true);
     if (this.loginForm.invalid) {
       this.loginForm.markAllAsTouched();
+      this.isSigningInSig.set(false);
       return;
     }
 
@@ -30,15 +38,19 @@ export class Login {
       next: (authUser) => {
         this.authService.setLanExamUser(authUser);
         this.router.navigate(['/student/dashboard']);
+        this.isSigningInSig.set(false);
 
         if (authUser.roles.includes('teacher')) {
           this.router.navigate(['/teacher/dashboard']);
-        } else if (authUser.roles.includes('s udent')) {
+        } else if (authUser.roles.includes('student')) {
           this.router.navigate(['/student/dashboard']);
         }
       },
-      error: () => {
-        this.loginForm.setErrors({ invalidLogin: true });
+      error: (err) => {
+        this.isSigningInSig.set(false);
+        this.errorMessageSig.set(
+          err.error.message || 'Login failed. Please try again.'
+        );
       },
     });
   }
