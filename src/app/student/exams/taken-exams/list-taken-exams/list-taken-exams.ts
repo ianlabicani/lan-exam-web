426c5abebf07e8cc6a46b3cefe0d79814ef586b5
ianlabicani <labicaniian@gmail.com>
2025-09-25 09:31:26 +0800

    make no use of the service

--- src/app/student/exams/taken-exams/list-taken-exams/list-taken-exams.ts
+++ src/app/student/exams/taken-exams/list-taken-exams/list-taken-exams.ts
@@ -1,6 +1,6 @@
 import { RouterLink } from '@angular/router';
-import { StudentTakenExamService } from './../../../services/student-taken-exam.service';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-taken-exams',
@@ -9,14 +9,71 @@ import { Component, inject, OnInit, signal } from '@angular/core';
   styleUrl: './list-taken-exams.css',
 })
 export class ListTakenExams implements OnInit {
-  studentTakenExamService = inject(StudentTakenExamService);
+  http = inject(HttpClient);
+
   takenExams = signal<any[]>([]);
 
   ngOnInit(): void {
-    this.studentTakenExamService.getAll().subscribe({
-      next: (res) => {
-        this.takenExams.set(res.data || []);
-      },
-    });
+    this.getTakenExams();
   }
+
+  getTakenExams() {
+    this.http
+      .get<{ data: TakenExam[] }>(
+        `http://127.0.0.1:8000/api/student/taken-exams`
+      )
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.takenExams.set(res.data);
+        },
+      });
+  }
+}
+
+interface TakenExam {
+  id: number;
+  exam_id: number;
+  type: string;
+  user_id: number;
+  started_at: Date;
+  submitted_at: null;
+  total_points: number;
+  created_at: Date;
+  updated_at: Date;
+  exam: Exam;
+}
+
+interface Exam {
+  id: number;
+  title: string;
+  description: string;
+  starts_at: Date;
+  ends_at: Date;
+  year: string;
+  sections: string[];
+  status: string;
+  total_points: number;
+  tos: To[];
+  created_at: Date;
+  updated_at: Date;
+}
+
+interface To {
+  topic: string;
+  outcomes: string[];
+  time_allotment: number;
+  no_of_items: number;
+  distribution: Distribution;
+}
+
+interface Distribution {
+  easy: Difficult;
+  moderate: Difficult;
+  difficult: Difficult;
+}
+
+interface Difficult {
+  allocation: number;
+  placement: any[];
 }
