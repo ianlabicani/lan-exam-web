78fcae775faaab80a943db6037e0ef63380da760
ianlabicani <labicaniian@gmail.com>
2025-10-17 15:24:56 +0800

    add route for the edit

--- src/app/teacher/exams/exams.routes.ts
+++ src/app/teacher/exams/exams.routes.ts
@@ -2,14 +2,19 @@ import { Routes } from '@angular/router';
 
 export const examsRoute: Routes = [
   {
-    path: 'exams',
-    loadComponent: () =>
-      import('./list-exams/list-exams').then((c) => c.ListExams),
-  },
-  {
     path: 'exams/create',
     loadComponent: () =>
       import('./create-exam/create-exam').then((c) => c.CreateExam),
+  },
+  {
+    path: 'exams/:examId/edit',
+    loadComponent: () =>
+      import('./edit-exam/edit-exam').then((c) => c.EditExam),
+  },
+  {
+    path: 'exams',
+    loadComponent: () =>
+      import('./list-exams/list-exams').then((c) => c.ListExams),
   },
   {
     path: 'exams/:examId',
